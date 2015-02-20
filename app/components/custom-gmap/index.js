3cd665d388212adede59a06cdf3246f040d7ec37
Einar Öberg <einar@northkingdom.com>
2015-02-20 21:28:28 +0100

    Added lego 3d-layer

--- app/components/custom-gmap/index.js
+++ app/components/custom-gmap/index.js
@@ -7,7 +7,10 @@ var textureOverlay = require('./texture-overlay');
 var Draggable = require('draggable');
 var THREE = require('three');
 var raf = require('raf');
-
+var BRIGL = require('brigl');
+var parts = require('parts');
+var TweenMax = require('tweenmax');
+var TimelineMax = require('timelinemax');
 var TILE_SIZE = 256;
 
 module.exports = {
@@ -16,10 +19,10 @@ module.exports = {
 
   ready: function() {
     this.gmapContainerEl = document.querySelector('.CustomGMap-container');
-    this.pegmanEl = document.querySelector('.CustomGMap-pegman');
+    this.minifigEl = document.querySelector('.CustomGMap-minifig');
     this.threeEl = document.querySelector('.CustomGMap-three');
 
-    _.bindAll(this, 'onStartDragPegman', 'onEndDragPegman','onDragPegman','drawStreetViewTileToCanvas','render');
+    _.bindAll(this, 'onStartDragMinifig', 'onEndDragMinifig','onDragMinifig','drawStreetViewTileToCanvas','render');
 
     gmapsUtils.load(this.loadMap.bind(this));
 
@@ -117,55 +120,120 @@ module.exports = {
       this.streetViewLayer = new google.maps.StreetViewCoverageLayer();
 
       this.init3D();
-      this.initPegman();
+      this.initMinifig();
 
       this.render();
     },
 
     render: function(){
-       this.rafId = raf(this.render);
-       this.renderer.render(this.scene,this.camera);
+      this.rafId = raf(this.render);
+      if(this.testMesh) {
+        //this.testMesh.rotation.y -= 0.005
+        //this.testMesh.brigl.animatedMesh.head.rotation.x += 0.01;
+      }
+
+      this.renderer.render(this.scene,this.camera);
     },
 
     init3D: function(){
       this.scene = new THREE.Scene();
 
       this.camera = new THREE.PerspectiveCamera(70, window.innerWidth/window.innerHeight, 1, 3100 );
-      this.camera.position.z = -100;
+      this.camera.position.y = 550;
+      //this.camera.position.z = -200;
       this.camera.lookAt(this.scene.position);
 
       this.renderer = new THREE.WebGLRenderer({alpha:true});
       this.renderer.setSize( window.innerWidth, window.innerHeight );
       this.renderer.sortObjects = false;
 
-      this.light = new THREE.DirectionalLight(0xffffff,0.6);
-      this.light.position.z = -50;
-      this.light.position.y = 100;
-      this.scene.add(this.light);
+      var light = new THREE.PointLight(0xffffff);
+      light.position.set(0,650,0);
+      this.scene.add(light);
 
-      this.scene.add( new THREE.Mesh(new THREE.SphereGeometry(10,10,10), new THREE.MeshBasicMaterial({color:0xff0000})));
+      light = new THREE.DirectionalLight(0xaaaaaa);
+      light.position.set(0,0,100);
+      this.scene.add(light);
+
+      this.brickContainer = new THREE.Object3D();
+      this.brickContainer.rotation.z = Math.PI;
+      this.scene.add(this.brickContainer);
+
+      //this.scene.add( new THREE.Mesh(new THREE.SphereGeometry(50,10,10), new THREE.MeshBasicMaterial({color:0xff0000})));
 
       this.threeEl.appendChild( this.renderer.domElement );
     },
 
-    initPegman: function(){
+    initMinifig: function(){
 
-      this.pegmanDraggingInstance = Draggable.create(this.pegmanEl, {
+      this.minifigDraggingInstance = Draggable.create(this.minifigEl, {
         type:"x,y",
         edgeResistance:0.5,
         throwProps:true,
         bounds:window,
-        onDragStart:this.onStartDragPegman,
-        onDragEnd:this.onEndDragPegman,
-        onDrag:this.onDragPegman
+        onDragStart:this.onStartDragMinifig,
+        onDragEnd:this.onEndDragMinifig,
+        onDrag:this.onDragMinifig
       })[0];
 
-      this.pegmanLocation = new google.maps.LatLng(0,0);
+      this.minifigLocation = new google.maps.LatLng(0,0);
+
+      var builder = new BRIGL.Builder("parts/ldraw/", parts, {dontUseSubfolders:true} );
+      var self = this;
+      //builder.loadModelFromLibrary("minifig.ldr", {drawLines: true}, function(mesh)
+      builder.loadModelByUrl("models/minifig.ldr", {drawLines: false}, function(mesh)
+      {
+        console.log(mesh)
+        //mesh.quaternion.setFromAxisAngle(new THREE.Vector3(1,1,-1).normalize(), 0);
+        //mesh.geometry.computeVertexNormals();
+        //mesh.geometry.applyMatrix( new THREE.Matrix4().makeRotationFromEuler( new THREE.Euler(0,Math.PI,0)));
+
+        mesh.rotation.x = Math.PI*-0.5;
+        mesh.rotation.z = Math.PI*0.5;
+        mesh.position.set(0,-300,0);
+        setTimeout(function(){
+          TweenMax.to(mesh.position,1,{delay:0.8,y:-200});
+
+          var delay = 0;
+          Object.keys( mesh.brigl.animatedMesh ).map(function( key ) {
+            delay+=0.05;
+            var origPos = mesh.brigl.animatedMesh[key].position.clone();
+            TweenMax.fromTo(mesh.brigl.animatedMesh[key].position, 1, {x:origPos.x*20,y:origPos.y*20,z:origPos.z*20, ease:Sine.easeOut},{x:origPos.x*2,y:origPos.y*2,z:origPos.z*2, ease:Sine.easeOut});
+            setTimeout(function(delay){
+              TweenMax.to(mesh.brigl.animatedMesh[key].position, 0.1, {overwrite:0,delay:1+delay,x:origPos.x,y:origPos.y,z:origPos.z, ease:Sine.easeIn});
+            }.bind(this,delay),      1000);
+
+            TweenMax.from(mesh.brigl.animatedMesh[key].rotation, 1  , {x:Math.PI*Math.random(),y:Math.PI*Math.random(),z:Math.PI*Math.random()*4, ease:Sine.easeOut});
+          });
+        },2000)
+
+
+
+        //var timeline = new TimelineMax()
+        //timeline
+        //TweenMax.to(mesh.rotation,2,{delay:1.2,x:0,y:0,z:0});
+
+        mesh.scale.set(1,1,1);
+
+        self.brickContainer.add(mesh);
+        self.testMesh = mesh;
+
+        //mesh.add( new THREE.VertexNormalsHelper(mesh,10));
+
+      }, function(err){
+
+        console.log(err)
+      });
+
+      /*request.get('/build/parts.js', function(res){
+        console.log(res);
+      });*/
+
     },
 
-    onDragPegman: function(event) {
+    onDragMinifig: function(event) {
 
-      var rect = this.pegmanEl.getBoundingClientRect()
+      var rect = this.minifigEl.getBoundingClientRect()
 
       var offset = {
         top: rect.top + document.body.scrollTop,
@@ -181,7 +249,7 @@ module.exports = {
       x = offset.left + 50,
       y = offset.top + 50
 
-      this.pegmanLocation = new google.maps.LatLng(
+      this.minifigLocation = new google.maps.LatLng(
         startLat + ((y/window.innerHeight) * (endLat - startLat)),
         startLng + ((x/window.innerWidth) * (endLng - startLng))
       );
@@ -189,7 +257,7 @@ module.exports = {
       var proj = this.map.getProjection();
 
       var numTiles = 1 << this.map.getZoom();
-      var worldCoordinate = proj.fromLatLngToPoint( this.pegmanLocation );
+      var worldCoordinate = proj.fromLatLngToPoint( this.minifigLocation );
 
       var pixelCoordinate = new google.maps.Point(
         worldCoordinate.x * numTiles,
@@ -219,27 +287,27 @@ module.exports = {
           if( trans > 0 && blue === 132 ) {
             validColor = true;
           }
-          if( validColor && !this.pegmanEl.classList.contains('road')) {
-            this.pegmanEl.classList.add('road');
+          if( validColor && !this.minifigEl.classList.contains('road')) {
+            this.minifigEl.classList.add('road');
           }
-          else if( !validColor && this.pegmanEl.classList.contains('road')){
-            this.pegmanEl.classList.remove('road');
+          else if( !validColor && this.minifigEl.classList.contains('road')){
+            this.minifigEl.classList.remove('road');
           }
         }
       }
     },
 
-    onStartDragPegman: function (){
+    onStartDragMinifig: function (){
 
       this.streetViewLayer.setMap(this.map);
 
       //$dragHideLayers.fadeOut()
-      this.pegmanEl.classList.add('dragging');
+      this.minifigEl.classList.add('dragging');
 
-      //pegmanTalk('Now drop me somewhere');
+      //minifigTalk('Now drop me somewhere');
     },
 
-    onEndDragPegman: function ( event ){
+    onEndDragMinifig: function ( event ){
 
       //remove streetview layer
       this.streetViewLayer.setMap();
@@ -247,26 +315,26 @@ module.exports = {
 
       //$('.js-bottom-instructions').html('Drag to look around');
 
-      //pegmanTalk('I hope there will be no snakes');
+      //minifigTalk('I hope there will be no snakes');
 
 
       //this.gmapContainerEl.classList.add('selected');
-      //_panoLoader.load(this.pegmanLocation);
-      //this.pegmanDraggingInstance.disable();
+      //_panoLoader.load(this.minifigLocation);
+      //this.minifigDraggingInstance.disable();
 
     },
 
-    pegmanTalk: function( msg, timeout ){
+    minifigTalk: function( msg, timeout ){
       /*$message.html(msg);
 
       TweenMax.fromTo($message,0.3,{x:0},{x:10,yoyo:true});
 
       if( timeout ) {
-        if( pegmanTimeout ) {
-          clearTimeout(pegmanTimeout);
+        if( minifigTimeout ) {
+          clearTimeout(minifigTimeout);
         }
-        pegmanTimeout = setTimeout(function(){
-          pegmanTalk(TALK_DEFAULT)
+        minifigTimeout = setTimeout(function(){
+          minifigTalk(TALK_DEFAULT)
         },timeout*1000)
       }*/
     },
