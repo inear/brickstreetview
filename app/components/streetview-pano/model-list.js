64b61b0473551fdc05c9939892cb233faa8eaaa8
Einar Öberg <einar@northkingdom.com>
2015-03-16 09:35:43 +0100

    Add missing files

---
+++ app/components/streetview-pano/model-list.js
@@ -0,0 +1,46 @@
+var THREE = require('three');
+
+module.exports = [
+  {
+    name:'sun.ldr',
+    callback: function(self,mesh) {
+
+      mesh.rotation.set(0,0,Math.PI*-0.5);
+      mesh.scale.set(1.1,1.1,1.1);
+      mesh.position.set(1420,1030,60);
+      self.scene.add(mesh);
+
+    }
+  },
+
+  //road plates
+  {
+    name:'44336p01.dat', callback:function(self,mesh) {
+      for (var j = 0; j < 4; j++) {
+        for (var i = 0; i < 2; i++) {
+
+          var newMesh = mesh.clone();
+          newMesh.rotation.set(0,0,Math.PI)
+
+          newMesh.scale.set(0.2,0.2,0.2);
+          newMesh.position.set(0,0,128+i*128);
+          self.nav.addRoad(j,newMesh);
+        }
+      }
+
+    }
+  },
+
+
+  //crossroad
+  {
+    name:'44343p01.dat',
+    callback: function(self,mesh) {
+      mesh.quaternion.setFromAxisAngle(new THREE.Vector3(1,0,-1).normalize(), Math.PI);
+      mesh.scale.set(0.20,0.20,0.20);
+      mesh.position.set(0,-20,0);
+      self.scene.add(mesh);
+
+    }
+  }
+]
