59a99ac883f3b3a896ee9dc6f99f993dcd59559c
Einar Öberg <einar@northkingdom.com>
2015-03-25 23:44:02 +0100

    Remove code

--- app/components/back-button/index.js
+++ app/components/back-button/index.js
@@ -5,15 +5,9 @@ var Vue = require('vue');
 
 module.exports = {
   replace: true,
-
-  data: function(){
-    return {
-
-    }
-  },
   template: fs.readFileSync(__dirname + '/template.html', 'utf8'),
-  methods:{
-    onClick: function(){
+  methods: {
+    onClick: function() {
       Vue.navigate('/map');
     }
   }
