d8537b293f25a104beb190fd03542729bb9ef234
Einar Öberg <einar@northkingdom.com>
2015-03-25 23:46:05 +0100

    File name change

---
+++ app/components/custom-gmap/gmap-styles.js
@@ -0,0 +1,58 @@
+module.exports = [
+  {'stylers': [
+    {'visibility': 'off'}
+  ]},
+  {
+    'featureType': 'landscape.man_made',
+    'stylers': [
+      {'visibility': 'on'},
+      {'color': '#6D6E5C'},
+      {'lightness': -75}
+    ]
+  },
+  {
+    featureType: 'road',
+    elementType: 'geometry',
+    stylers: [
+      {'visibility': 'simplified'},
+      {color: '#6C6E68'}
+    ]
+  },
+  {
+    featureType: 'landscape.natural',
+    elementType: 'geometry',
+    stylers: [
+      {color: '#6D6E5C'}
+    ]
+  },
+  {
+    'featureType': 'landscape.man_made',
+    'stylers': [
+      {'visibility': 'simplified'}
+    ]
+  },
+  {
+    'featureType': 'road',
+    'elementType': 'labels.text.fill',
+    'stylers': [
+      {'visibility': 'on'},
+      {'color': '#FFF03A'}
+    ]
+  },
+  {
+    'featureType': 'water',
+    'elementType': 'geometry.fill',
+    'stylers': [
+      {'visibility': 'on'},
+      {'color': '#4C61DB'}
+    ]
+  },
+  {
+    'featureType': 'landscape.natural',
+    'elementType': 'geometry.fill',
+    'stylers': [
+      {'visibility': 'on'},
+      {'color': '#6D6E5C'}
+    ]
+  }
+];
